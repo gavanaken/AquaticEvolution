100bafe3b9fe2930ea86d7d0cb679763142d02fd
greg van aken <greg.vanaken@mobilionsystems.com>
2020-07-16 22:12:33 -0400

    Initial project from forge

---
+++ src/main/java/com/vanalia/aquaticevolution/AquaticEvolution.java
@@ -0,0 +1,35 @@
+package com.vanalia.aquaticevolution;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("aqe")
+public class AquaticEvolution
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public AquaticEvolution() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event){
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
